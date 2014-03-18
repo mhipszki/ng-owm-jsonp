4c38aeaf177fd8b4df71f7b0d56575fabbc9734d
Marton Hipszki <mhipszki@gmail.com>
2014-03-18 19:59:43 +0000

    retrieved weather data is shown now
    - using Bootstrap widgets
    - received data must be processed (data conversion etc.)
    - on any kind of error a warning message is shown to the user

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,12 +5,50 @@ var app = angular.module('owmClient', ['ui.bootstrap']);
 app.controller('owmClient.controller', function($scope, config, owmAPI) {
   $scope.config = config;
 
-  $scope.updateWeatherOf = function(city) {
-    owmAPI.getCurrentWeatherInfoOf(city, showWeatherInfo);
+  $scope.weather = {};
+
+  $scope.hideWeatherData = function() {
+    $scope.weather = {};
   };
 
-  var showWeatherInfo = function(data){
-    console.log(data);
+  $scope.updateWeatherOf = function(city) {
+    owmAPI.getCurrentWeatherInfoOf(city, storeWeather, showError);
+  };
+
+  var storeWeather = function(data){
+    $scope.weather = processWeather(data);
+  };
+
+  var showError = function(data, status, headers, config) {
+    $scope.weather.hasError = true;
+  };
+
+  var processWeather = function(data) {
+    var weather = {};
+
+    weather.isRetrieved = parseInt(data.cod)===200;
+    weather.hasError = !weather.isRetrieved;
+
+    var current = angular.extend({}, data.weather[0]);
+    current.wind = data.wind;
+    current.temperature = toCelsius(data.main.temp);
+    current.range = {
+      min: toCelsius(data.main.temp_min),
+      max: toCelsius(data.main.temp_max)
+    };    
+
+    weather.data = data;
+    weather.current = current;
+
+    return weather;
+  };
+
+  var toCelsius = function(kelvin) {
+    return roundToDecimal(kelvin - 273.15);
+  };
+
+  var roundToDecimal = function(value) {
+    return Math.round(value*10)/10;
   };
 });
 
@@ -28,7 +66,7 @@ app.constant('config', {
 });
 
 app.service('owmAPI', function($http, config) {
-  this.getCurrentWeatherInfoOf = function(city, onSuccess) {
-    $http.jsonp(config.apiUrl.replace(config.cityToken, city)).success( onSuccess );
+  this.getCurrentWeatherInfoOf = function(city, onSuccess, onError) {
+    $http.jsonp(config.apiUrl.replace(config.cityToken, city)).success( onSuccess ).error( onError );
   };
 });
