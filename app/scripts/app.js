fd8931f4c715e704c162865ef928b803ce36c6fd
Marton Hipszki <mhipszki@gmail.com>
2014-03-18 17:54:48 +0000

    added owmAPI service to retrieve OWM weather data through a JSONP request

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,16 @@
 
 var app = angular.module('owmClient', ['ui.bootstrap']);
 
-app.controller('owmClient.controller', function($scope, config) {
+app.controller('owmClient.controller', function($scope, config, owmAPI) {
   $scope.config = config;
+
+  $scope.updateWeatherOf = function(city) {
+    owmAPI.getCurrentWeatherInfoOf(city, showWeatherInfo);
+  };
+
+  var showWeatherInfo = function(data){
+    console.log(data);
+  };
 });
 
 app.constant('config', {
@@ -14,5 +22,13 @@ app.constant('config', {
     'Manchester',
     'Birmingham'
   ],
-  selectedCity: ''
+  selectedCity: '',
+  apiUrl: 'http://api.openweathermap.org/data/2.5/weather?q={{city}},UK&callback=JSON_CALLBACK',
+  cityToken: '{{city}}'
+});
+
+app.service('owmAPI', function($http, config) {
+  this.getCurrentWeatherInfoOf = function(city, onSuccess) {
+    $http.jsonp(config.apiUrl.replace(config.cityToken, city)).success( onSuccess );
+  };
 });
