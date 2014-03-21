f917c6cf7a05c2f00f0aa61583caa4944e0857ec
Marton Hipszki <mhipszki@gmail.com>
2014-03-21 12:57:50 +0000

    added integration tests for city buttons

--- test/e2e/index.spec.js
+++ test/e2e/index.spec.js
@@ -20,4 +20,26 @@ describe("owm client", function() {
   it("should display all 4 cities as a button", function() {
     expect(page.cities.count()).toBe(4);
   });
+
+  it("should highlight a button on click", function() {
+    page.cities.get(0).then( function(button) {
+      button.click();
+      expect(button.getAttribute('class')).toMatch(/active/);
+    });
+  });
+
+  it("should highlight only one button at a time", function() {
+    page.cities.get(0).then( function(button) {
+      button.click();
+      expect(button.getAttribute('class')).toMatch(/active/);
+
+      page.cities.get(1).then( function(otherButton) {
+        otherButton.click();
+        expect(otherButton.getAttribute('class')).toMatch(/active/);
+        expect(button.getAttribute('class')).not.toMatch(/active/);
+      });
+
+    });
+  });
+
 });
