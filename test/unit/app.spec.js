84c3b2d57e09092806d1a2e5d406f13594a41d6e
Marton Hipszki <mhipszki@gmail.com>
2014-03-24 17:08:21 +0000

    added more tests to owlClient/controller test suit

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -2,33 +2,57 @@
 
 describe('owmClient', function () {
 
-  beforeEach(module('owmClient'));
+  describe('controller', function() {
 
-  var scope;
-  var controller;
-  var config;
+    beforeEach(module('owmClient'));
 
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    controller = $controller('owmClient.controller', {
-      $scope: scope
+    var scope;
+    var controller;
+    var config;
+    var weatherService;
+
+    beforeEach(inject(function ($controller, $rootScope, _weatherService_) {
+      scope = $rootScope.$new();
+      controller = $controller('owmClient.controller', {
+        $scope: scope
+      });
+      config = scope.config;
+      weatherService = _weatherService_;
+    }));
+
+    it('should define the app title', function () {
+      expect(config.title.length).toBeGreaterThan(0);
     });
-    config = scope.config;
-  }));
 
-  it('should define the app title', function () {
-    expect(config.title.length).toBeGreaterThan(0);
-  });
+    it('should define four UK city names', function() {
+      expect(config.cities.length).toBe(4);
+    });
 
-  it('should define four UK city names', function() {
-    expect(config.cities.length).toBe(4);
-  });
+    it('should have London in cities', function() {
+      expect(config.cities).toContain('London');
+    });
 
-  it('should have London in cities', function() {
-    expect(config.cities).toContain('London');
-  });
+    it('should not have a city selected at first', function() {
+      expect(config.selectedCity).toBe('');
+    });
 
-  it('should not have a city selected at first', function() {
-    expect(config.selectedCity).toBe('');
+    it('should have an empty weather object defined', function() {
+      expect(scope.weather).toEqual({});
+    });
+
+    it('should clear weather data when hideWeatherData() called', function() {
+      scope.weather.testValue = 1;
+      scope.hideWeatherData();
+      expect(scope.weather.testValue).toBeUndefined();
+    });
+
+    it('should use weatherService to gather weather information', function() {
+      spyOn(weatherService, 'getCurrentWeatherOf');
+      var city = 'a UK city';
+      scope.updateWeatherOf(city);
+      var service = weatherService.getCurrentWeatherOf;
+      expect(service).toHaveBeenCalled();
+      expect(service.mostRecentCall.args[0]).toBe(city);
+    });
   });
 });
